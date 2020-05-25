a5bde29f0c729da7fe2e83467f4772d22aaae3ff
David Villegas <david.villegasg7@gmail.com>
2020-05-25 16:09:37 -0400

    Creating edit profile view & styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+  # before_action :set_user. only: [:edit]
+
 
   def show
+    @user = User.find(params[:id])
+  end
 
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 
 end
