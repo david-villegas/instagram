ab327828301ac8ac980f7faf86cb358869bd84ed
David Villegas <david.villegasg7@gmail.com>
2020-05-25 18:03:27 -0400

    Creating update profile & alerts for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user), notice: '¡ Perfil actualizado correctamente !'
+    else
+      render :edit, alert: '¡ Error al actualizar, intente nuevamente !'
+    end
+  end
   private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+  end
 
   def set_user
     @user = User.find(params[:id])
