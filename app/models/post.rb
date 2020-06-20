abbf108a73242916bcb0e5f564703517d3de5923
David Villegas <david.villegasg@gmail.com>
2020-06-19 20:41:37 -0400

    Creating search top hashtags

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,15 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validates :description, :image, presence: true
+  after_commit :create_hash_tags, on: :create
 
+  def create_hash_tags
+    extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+
+  def extract_name_hash_tags
+    description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+  end
 end
